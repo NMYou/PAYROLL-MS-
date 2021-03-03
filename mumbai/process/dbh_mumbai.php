66bc2b2b156b5769ba03eb825d282c5071cba654
NMYou <77392610+NMYou@users.noreply.github.com>
2021-03-04 01:07:57 +0530

    update database to remote connection and directory changes

--- mumbai/process/dbh_mumbai.php
+++ mumbai/process/dbh_mumbai.php
@@ -1,9 +1,15 @@
 <?php
+//Development Connection
+// $servername = "localhost";
+// $dBUsername = "root";
+// $dbPassword = "";
+// $dBName = "payroll_mumbai";
 
-$servername = "localhost";
-$dBUsername = "root";
-$dbPassword = "";
-$dBName = "payroll_mumbai";
+//Remote Database Connection
+$servername = "remotemysql.com";
+$dBUsername = "maMc1saxdP";
+$dbPassword = "FQ9Mg3GdUu";
+$dBName = "maMc1saxdP";
 
 $conn = mysqli_connect($servername, $dBUsername, $dbPassword, $dBName);
 
